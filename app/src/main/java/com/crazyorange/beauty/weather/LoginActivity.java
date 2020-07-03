21b21653835eccc093b3acf0872f946a2dad52ca
842917077@qq.com <842917077@qq.com>
2020-07-03 10:24:51 +0800

    Beauty Weather
    Build  Beauty-Weather project architecture

---
+++ app/src/main/java/com/crazyorange/beauty/weather/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.crazyorange.beauty.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author CrazyOrange
+ *
+ * Orange Weather Login Page
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
